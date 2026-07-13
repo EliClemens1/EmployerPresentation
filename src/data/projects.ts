4fa07d751e1f5d553d57eb07f6c94414834a437b
Eli Clemens <ec02162129@otc.edu>
2026-07-13 15:42:21 -0500

    added project.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,7 @@ export interface Project {
     description: string
     technologies: string[]
     github: string
+    URL: string
   }
   
   export const projects: Project[] = [
@@ -12,6 +13,7 @@ export interface Project {
         '2D action game built with Godot and C# featuring enemies, bosses, upgrades, and progression.',
       technologies: ['C#', 'Godot', 'Git'],
       github: 'https://github.com/EliClemens/ZotarianEscape',
+      URL: "N/A",
     },
   
     {
@@ -20,5 +22,14 @@ export interface Project {
         'Vue and Firebase application for managing and displaying artwork.',
       technologies: ['Vue', 'Firebase', 'JavaScript'],
       github: 'https://github.com/EliClemens1/ArtPortfolio',
+      URL: "https://artist-s-portfolio.web.app/",
     },
+    {
+      title: 'EmployerPresentation',
+      description:
+        'Vue.js web application to display my current skills and accomplishments.',
+        technologies: ['HTML', 'CSS', 'Vue.js'],
+        github: 'https://github.com/EliClemens1/EmployerPresentation.git',
+        URL: 'N/A'
+    }
   ]
